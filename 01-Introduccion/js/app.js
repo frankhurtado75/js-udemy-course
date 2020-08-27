0f2a6fe55e3cb049125e097a28ec85f82ad5ee1c
frankhurtado75 <frankhurtado75@gmail.com>
2020-08-27 09:29:21 -0400

    introduccion

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,39 @@
+console.log('Hola');
+console.log('Mundo');
+
+function hola() {
+    console.log('ok');
+    console.log('Hola'); 
+    console.log('Mundo');
+}
+
+//prompt
+prompt('Como te llamas?');
+
+//Alerta
+alert('Error');
+
+//Confirmacion
+confirm('Desea borrar este elemento?');
+
+//Arreglos
+console.log([1,2,3]);
+
+//Arreglos de tablas
+console.table([1,2,3]);
+
+//Error de consola
+console.error('Error');
+
+//Advertencia
+console.warn('Advertencia');
+
+//Tiempo de ejecusion de codigo
+console.time('hola'); 
+console.warn('Advertencia');
+console.warn('Advertencia');
+console.warn('Advertencia');
+console.warn('Advertencia');
+console.warn('Advertencia');
+console.warn('Advertencia');
+console.timeEnd('hola');
