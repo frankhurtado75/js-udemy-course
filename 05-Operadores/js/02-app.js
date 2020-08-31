007b4375c6886f992898aa8042e660190a4c81a7
frankhurtado75 <frankhurtado75@gmail.com>
2020-08-31 15:17:02 -0400

    operadores

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,22 @@
+//Comparacion de valores
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+// Revisar si 2 números son iguales
+console.log(numero1 == numero3); //False
+console.log(numero1 == numero2); //True
+
+// Comparador estricto, === compara el tipo de dato
+console.log(numero1 === parseInt(numero2)); //True
+console.log(numero1 === numero2); //False
+console.log(typeof numero1 );
+console.log(typeof numero2 );
+
+// Diferente
+const password1 = "admin";
+const password2 = "Admin";
+
+console.log(password1 != password2); //True
+console.log(numero1 != numero2); //False
+console.log(numero1 !== parseInt(numero2)); //False
---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,15 @@
+//Comparar null y undefined
+
+// Undefined
+let numero;
+console.log(numero);  //Undefined
+console.log(typeof numero); //Undefined
+
+// Null
+let numero2 = null;
+console.log(numero2); //Null
+console.log(typeof numero2); //Object
+
+//Comparacion
+console.log(numero == numero2); //True
+console.log(numero === numero2); //False
