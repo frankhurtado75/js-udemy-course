425a8713d928e619f3754f4843a4d97d425bdf71
frankhurtado75 <frankhurtado75@gmail.com>
2020-10-03 22:57:16 -0400

    proyecto enviarEmail

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,123 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+const btnReset = document.querySelector('#resetBtn');
+const formulario = document.querySelector('#enviar-mail');
+
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+// Expresion regular para validar email
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// Event Listeners
+eventListeners();
+
+function eventListeners(){
+    // Cuando la app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campo del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
+    // Reinicia el formulario
+    btnReset.addEventListener('click', resetForm);
+
+    // Enviar email
+    formulario.addEventListener('submit', enviarEmail);
+}
+
+// Functions
+function iniciarApp(){
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// Validar el formulario
+function validarFormulario(e){
+    if(e.target.value.length > 0){
+        // Elimina los errores
+        const error = document.querySelector('p.error');
+        if(error){
+            error.remove();
+        }
+
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+    } else {
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if(e.target.type === 'email'){
+        if(er.test(e.target.value)){
+            // Elimina los errores
+            const error = document.querySelector('p.error');
+            if(error){
+                error.remove();
+            }
+            
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+            mostrarError('Email no válido');
+        }
+    }
+
+    if(er.test(email.value) !== '' && asunto.value !== '' && mensaje.value !== ''){
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
+    }
+}
+
+// Muestra el error en el formulario
+function mostrarError(mensaje){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if(errores.length === 0){
+        formulario.appendChild(mensajeError);
+    }
+}
+
+// Envia el email
+function enviarEmail(e){
+    e.preventDefault()
+    
+    // Mostrar el spinner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'flex';
+
+    // Despues de 3 segundos ocultar el spinner y mnostrar el mensaje
+    setTimeout(() => {
+        spinner.style.display = 'none';
+
+        // Mensaje que dice que se envió correctamente
+        const parrafo = document.createElement('p');
+        parrafo.textContent = 'El mensaje se envió correctamente';
+        parrafo.classList.add('text-center', 'my-10', 'p-2', 'bg-green-500', 'text-white', 'font-bold', 'uppercase');
+
+        // Inserta el parrafo antes del spinner
+        formulario.insertBefore(parrafo, spinner);
+
+        // Elimina el mensaje de éxito
+        setTimeout(() => {
+            parrafo.remove();
+            resetForm();
+        }, 10000)
+    }, 3000 ) // Cada 1000 es un segundo
+}
+
+// Funcion que resetea el formulaio
+function resetForm(){
+    formulario.reset();
+    iniciarApp();
+}
