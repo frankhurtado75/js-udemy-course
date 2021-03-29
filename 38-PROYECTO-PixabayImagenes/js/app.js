77e7215b4b417b94a0b1f1d752a9faeca4dc1300
frankhurtado75 <frankhurtado75@gmail.com>
2021-03-29 19:38:19 -0400

    Proyecto Buscador de Imagenes con pixabay 1.1

---
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -0,0 +1,81 @@
+// Variables
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+const paginacion = document.querySelector('#paginacion');
+
+// Events
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+}
+
+// Funtions
+function validarFormulario(e){
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if(terminoBusqueda === ''){
+        mostrarAlerta('Agrega un término de búsuqeda');
+        return;
+    }
+
+    buscarImagenes(terminoBusqueda);
+}
+
+function mostrarAlerta(mensaje){
+
+    const existeAlerta = document.querySelector('.bg-red-100');
+
+    if(!existeAlerta){
+        const alerta = document.createElement('p');
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+    
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block sm:inline">${mensaje}</span>
+        `;
+    
+        formulario.appendChild(alerta);
+    
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    } 
+}
+
+function buscarImagenes(termino){
+    const key = '20828950-8467dacb2ad66218536d72a79';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=8`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            mostrarImagenes(resultado.hits);
+        })
+}
+
+function mostrarImagenes(imagenes){
+    console.log(imagenes);
+
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y construir el html
+    imagenes.forEach(imagen => {
+        const {previewURL, likes, views, largeImageURL} = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}"></img>
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light"> Me gusta</span></p>
+                        <p class="font-bold">${views} <span class="font-light"> veces vistas</span></p>
+                        <a href="${largeImageURL}" target="_blank" rel="noopener noreferrer" class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1">Ver imagen</a>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+}
