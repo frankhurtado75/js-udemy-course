a6cfc1bb2a55e13c4e41550029acf70650a28782
frankhurtado75 <frankhurtado75@gmail.com>
2021-03-31 22:04:17 -0400

    Proyecto Criptomonedas 0.1

---
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -0,0 +1,33 @@
+// Variables
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+
+// Promise
+const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
+    resolve(criptomonedas);
+});
+
+// Event
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomoneda();
+})
+
+// Functions
+function consultarCriptomoneda(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => obtenerCriptomonedas(resultado.Data))
+        .then(criptomonedas => selectCriptomonedas(criptomonedas))
+}
+
+function selectCriptomonedas(criptomonedas){
+    criptomonedas.forEach(cripto => {
+        const {FullName, Name} = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+    })
+}
