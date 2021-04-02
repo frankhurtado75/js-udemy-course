9c71a7ad0672b6833382111d00333f9ce57ae74f
frankhurtado75 <frankhurtado75@gmail.com>
2021-04-02 17:33:14 -0400

    Proyecto Criptomonedas 1.0

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -2,6 +2,7 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
 const monedaSelect = document.querySelector('#moneda');
 const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
 
 const objBusqueda = {
     moneda: '',
@@ -56,11 +57,92 @@ function submitFormulario(e){
     const {moneda, criptomoneda} = objBusqueda;
 
     if(moneda === '' || criptomoneda === ''){
-        mostrarAlerta('Amnbos campos son obligatorios');
+        mostrarAlerta('Ambos campos son obligatorios');
         return;
     }
+
+    // Consultar la API con los resultados
+    consultarAPI();
 }
 
 function mostrarAlerta(msg){
-    const divMensaje = document.createElement.add
+
+    // Mostrar un solo error
+    const existeError = document.querySelector('.error');
+
+    if(!existeError){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('error');
+
+        // Mensaje error
+        divMensaje.textContent = msg;
+        formulario.appendChild(divMensaje);
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+    
+}
+
+function consultarAPI(){
+    const {moneda, criptomoneda} = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+
+    mostrarSpinner();
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(cotizacion => {
+            mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+        })
+}
+
+function mostrarCotizacionHTML(cotizacion){
+
+    limpiarHTML();
+
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = cotizacion;
+
+    const precio = document.createElement('p');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('p');
+    precioAlto.innerHTML = `<p>Precio más alto del día <span>${HIGHDAY}</span></p>`
+
+    const precioBajo = document.createElement('p');
+    precioBajo.innerHTML = `<p>Precio más bajo del día <span>${LOWDAY}</span></p>`
+
+    const ultimasHoras = document.createElement('p');
+    ultimasHoras.innerHTML = `<p>Variación en las últimas 24 horas <span>${CHANGEPCT24HOUR}%</span></p>`
+
+    const ultimaActualizacion = document.createElement('p');
+    ultimaActualizacion.innerHTML = `<p>Últimas actualización <span>${LASTUPDATE}</span></p>`
+
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(ultimasHoras);
+    resultado.appendChild(ultimaActualizacion);
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function mostrarSpinner(){
+    limpiarHTML();
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+    spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `
+
+    resultado.appendChild(spinner);
 }
